79375f62122eaea145293e43f5232ff448afd3f9
Adri Oliveira <84540574+aoliveiraa@users.noreply.github.com>
2021-06-09 14:40:49 -0300

    Update startApi.ps1

--- .github/scripts/startApi.ps1
+++ .github/scripts/startApi.ps1
@@ -26,4 +26,13 @@ Start-Sleep -Seconds 15
 
 Receive-Job $actor
 
+#start test 
+$test = Start-Job -ScriptBlock {
+    opentest session create --template "Call API Template" --wait --out junit.xml
+}
+
+Start-Sleep -Seconds 15
+
+Receive-Job $test
+
 exit 0
