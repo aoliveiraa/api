99bd23eb4d8322cb3dd01fb73cf265fbe6ba2c07
Adri Oliveira <84540574+aoliveiraa@users.noreply.github.com>
2021-06-09 13:04:25 -0300

    Add files via upload

---
+++ .github/scripts/startApi.ps1
@@ -0,0 +1,34 @@
+$ErrorActionPreference = "Stop"
+
+#start dotnet application
+$dotnet = Start-Job -ScriptBlock {
+    dotnet run src/ApiReservas
+}
+
+Start-Sleep -Seconds 10
+
+Receive-Job $dotnet
+
+#start server 
+$server = Start-Job -ScriptBlock {
+    cd .\opentest\server
+    opentest server
+}
+
+Start-Sleep -Seconds 15
+
+Receive-Job $server
+
+#start actor 
+$actor = Start-Job -ScriptBlock {
+    cd .\opentest\actor-web
+    opentest actor
+}
+
+Start-Sleep -Seconds 15
+
+Receive-Job $actor
+
+& opentest session create --template "Call API Template" --wait --out junit.xml
+
+exit 0
