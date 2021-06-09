2abc6d9e599f3e24b3943b8ba05113e471ed946b
Adri Oliveira <84540574+aoliveiraa@users.noreply.github.com>
2021-06-09 15:01:07 -0300

    Update startApi.ps1

--- .github/scripts/startApi.ps1
+++ .github/scripts/startApi.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = "Stop"
 
 #start dotnet application
 $dotnet = Start-Job -ScriptBlock {
-    dotnet run /src/ApiReservas
+    dotnet run $using:BasePath\src\ApiReservas
 }
 
 Start-Sleep -Seconds 10
